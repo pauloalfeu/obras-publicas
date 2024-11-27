636da69119c7ba349b3e3a82023a358112f7c5fc
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-27 13:26:45 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -218,7 +218,6 @@ if uploaded_file is not None:
         plt.imshow(wordcloud, interpolation='bilinear')
         plt.max_font_size = 150
         plt.axis('off')
-        plt.show()
         st.pyplot(plt)
         st.markdown("A imagem acima representa uma visualização das palavras mais frequentes encontradas nas descrições das obras públicas. Após um processo de limpeza e contagem, as palavras mais comuns, como ""lote"", ""municipal"" e ""pública"", foram removidas para destacar os termos mais relevantes e específicos. Essa nuvem de palavras oferece uma visão geral dos temas e características mais comuns presentes nas obras analisadas.")
 
