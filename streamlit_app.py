379b874c9a5e56dcb49a2cc7abcbb5f6a601185c
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-27 16:46:03 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,13 @@ import re
 st.set_page_config(page_title="Obras Publicas")
 
 st.sidebar.markdown('''
-# Sections
+<style>
+a {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
+# Navegue pela página:
 - [Página Inicial](#section-1)
 - [Conceitos, Tabelas e Gráficos](#section-2)
 - [Escolha da base de dados](#section-3)
