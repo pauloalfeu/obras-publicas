c0591bb5e47152e7c5efc657cc6ea4caa4ad003c
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-27 13:08:48 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 from wordcloud import WordCloud
 import PIL.Image as Image
 from collections import Counter
