5bce422a81a10a9ead8b25594b2184b30fc4ad1a
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-27 10:04:49 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,14 +13,17 @@ st.markdown("### 🏗️ ANÁLISE DO ANDAMENTO DE OBRAS PÚBLICAS UTILIZANDO DAD
 tab1, tab2 = st.tabs(["Obra Pública", "Tutorial - Obtendo dados no Portal da Transparência de Cascavel "])
 
 with tab1:
-    st.markdown("#### Obra Pública: Conceito e Características")
-    st.image("https://egob.com/wp-content/uploads/2021/01/egob_obra_publica-1024x671.png", width=300)
-    st.markdown("De acordo com o Conselho Nacional do Ministério Público, obra pública é considerada “toda construção, reforma, fabricação, recuperação ou ampliação de bem público” (CNMP, 2017).")
-    st.markdown("Ressalte-se que a qualidade de uma obra depende do adequado gerenciamento de suas diversas etapas intermediárias e da participação de profissionais capacitados (CNMP, 2017).")
-    st.markdown("#### Lei de Acesso à Informação")
-    st.image("https://goias.gov.br/educacao/wp-content/uploads/sites/40/2024/03/lei.png", width=400)
-    st.markdown(" A Lei de Acesso à Informação Nº 12.527, foi sancionada em 18 de novembro de 2011. Esta tem como diretriz o princípio de publicidade máxima da administração pública. ")
-    st.markdown("O artigo 3º da lei prevê a utilização de ferramentas de comunicação viabilizadas pela tecnologia da informação. Uma das principais manifestações da utilização da T.I. na publicidade dos dados públicos está no chamados Portais da Transparência.")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown("#### Obra Pública: Conceito e Características")
+        st.image("https://egob.com/wp-content/uploads/2021/01/egob_obra_publica-1024x671.png", width=300)
+        st.markdown("De acordo com o Conselho Nacional do Ministério Público, obra pública é considerada “toda construção, reforma, fabricação, recuperação ou ampliação de bem público” (CNMP, 2017).")
+        st.markdown("Ressalte-se que a qualidade de uma obra depende do adequado gerenciamento de suas diversas etapas intermediárias e da participação de profissionais capacitados (CNMP, 2017).")
+    with col2:
+        st.markdown("#### Lei de Acesso à Informação")
+        st.image("https://goias.gov.br/educacao/wp-content/uploads/sites/40/2024/03/lei.png", width=400)
+        st.markdown(" A Lei de Acesso à Informação Nº 12.527, foi sancionada em 18 de novembro de 2011. Esta tem como diretriz o princípio de publicidade máxima da administração pública. ")
+        st.markdown("O artigo 3º da lei prevê a utilização de ferramentas de comunicação viabilizadas pela tecnologia da informação. Uma das principais manifestações da utilização da T.I. na publicidade dos dados públicos está no chamados Portais da Transparência.")
     st.markdown("")
     st.markdown("###### Definição de Portal da Transparência:")
     st.markdown(">\"é um site de acesso livre, no qual o cidadão pode encontrar informações sobre como o dinheiro público é utilizado, além de se informar sobre assuntos relacionados à gestão pública\" (Controladoria Geral da União).")
