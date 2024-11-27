9ee8919dbd04ee2ca889c7127dda8791a2245fe0
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-27 14:04:06 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -219,7 +219,8 @@ if uploaded_file is not None:
         plt.imshow(wordcloud, interpolation='bilinear')
         plt.max_font_size = 150
         plt.axis('off')
-        plt.title("Nuvem de Palavras-Chave: Descrições das Obras Públicas", fontsize=18, fontweight='bold', color='#20201e')
+        plt.title("Nuvem de Palavras-Chave: Descrições das Obras Públicas", fontsize=18, fontweight='normal', color='#20201e', fontfamily='Times New Roman')
+        plt.text(0.5, -0.1, "Imagem gerada a partir da biblioteca Python: Wordcloud", ha='center', transform=plt.axes().transAxes)
         st.pyplot(plt)
         st.markdown("A imagem acima representa uma visualização das palavras mais frequentes encontradas nas descrições das obras públicas. Após um processo de limpeza e contagem, as palavras mais comuns, como ""lote"", ""municipal"" e ""pública"", foram removidas para destacar os termos mais relevantes e específicos. Essa nuvem de palavras oferece uma visão geral dos temas e características mais comuns presentes nas obras analisadas.")
 
