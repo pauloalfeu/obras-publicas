81796b7fbab86d05c56188b367743e51070d3250
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-12-09 02:54:21 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -374,10 +374,12 @@ if uploaded_file is not None:
         </figure>
         </div>
         """, unsafe_allow_html=True)
-        st.markdown("""A análise de agrupamento K-means revelou três perfis distintos de obras.
-        * *Grupo Azul:* Geralmente, obras de menor duração e com alto percentual de conclusão. Podem ser projetos menores ou com maior prioridade.
-        * *Grupo Verde:* Obras com tempo de execução médio e percentual de conclusão variável. Podem representar um grupo mais heterogêneo de projetos.
-        * *Grupo Amarelo:* Obras de maior duração e, em alguns casos, com menor percentual de conclusão. Podem indicar projetos mais complexos ou com atrasos.
+        st.markdown("""
+        A análise de agrupamento K-means revelou três perfis distintos de obras.
+
+        * **Grupo Azul:** Geralmente, obras de menor duração e com alto percentual de conclusão. Podem ser projetos menores ou com maior prioridade.
+        * **Grupo Verde:** Obras com tempo de execução médio e percentual de conclusão variável. Podem representar um grupo mais heterogêneo de projetos.
+        * **Grupo Amarelo:** Obras de maior duração e, em alguns casos, com menor percentual de conclusão. Podem indicar projetos mais complexos ou com atrasos.
         """)
 
 
