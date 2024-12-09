9ae18fa77b69af6c42986d5504db709d06b5d423
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-12-09 01:10:26 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -245,7 +245,7 @@ if uploaded_file is not None:
     with tab3:
         #Andamento_per_year
         # Gráfico Total de obras concluídas com menos de 100% por ano
-        sns.set_style("white")  # Estilo base
+        #sns.set_style("white")  # Estilo base
         sns.set(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})  # Fundo transparente
 
         sns.barplot(x='Ano', hue='Ano', y= 'Qtd', data=Not100_by_year, palette='Set2', legend=False, gap=0.1, width=0.8)
